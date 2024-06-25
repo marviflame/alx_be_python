58fc6a5feba3923b63b46fcd772077e3dd5c5d23
marviflame <chalcedony800@gmail.com>
2024-06-25 10:42:29 -0700

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# This program does financial calculations
+
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+projected_saving = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print("Your monthly savings are:", monthly_savings)
+print("Projected savings after one year, with interest, is", projected_saving)
