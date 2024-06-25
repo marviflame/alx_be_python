9add6f3d4b34a83a522228a425da915b40bae588
marviflame <chalcedony800@gmail.com>
2024-06-25 10:49:43 -0700

    python introduction

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+# This program converts hours to seconds
+
+hours = 2
+seconds = hours * 3600
+
+print(hours, "hour(s) is", seconds, "seconds.")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+# This program calculates the rectangle of a given length and breath
+
+length = 10
+width = 5
+
+area = length * width
+
+print("The area of the rectangle is:", area)
