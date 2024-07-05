f397a06c92f6ded6bfd999c8b8ba59b3b0c0d74e
marviflame <chalcedony800@gmail.com>
2024-07-05 08:01:54 -0700

    advance

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = int(input ("Enter your monthly income: "))
 monthly_expenses = int(input ("Enter your total monthly expenses: "))
-monthly_savings = (monthly_income - monthly_expenses)
+monthly_savings = (float(monthly_income) - float(monthly_expenses))
 print (("Your monthly savings are: "), (monthly_savings))
 
 projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
